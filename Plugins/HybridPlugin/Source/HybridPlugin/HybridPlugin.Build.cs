66d9a5d5cfaa3edef4140ccb08d06e60c096e52e
Nate West <nwestr@gmail.com>
2017-09-01 11:57:24 -0400

    Add Hybrid plugin with embedded framework

---
+++ Plugins/HybridPlugin/Source/HybridPlugin/HybridPlugin.Build.cs
@@ -0,0 +1,65 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class HybridPlugin : ModuleRules
+{
+	public HybridPlugin(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"HybridPlugin/Public"
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"HybridPlugin/Private",
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+
+        if (Target.Platform == UnrealTargetPlatform.IOS) {
+            PublicAdditionalFrameworks.Add(
+                new UEBuildFramework(
+                    "Hybrid",
+                    "../ThirdParty/Hybrid.embeddedframework.zip",
+                    "./Hybrid.bundle"
+                )
+            );
+        }
+	}
+}
--- Source/Hybrid/Hybrid.Build.cs
+++ Source/Hybrid/Hybrid.Build.cs
@@ -8,7 +8,7 @@ public class Hybrid : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HybridPlugin" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
